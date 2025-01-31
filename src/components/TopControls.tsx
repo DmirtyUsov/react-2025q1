f46c79a21fa1a6c3fe64999f9458b55de8e98e3a
Dima Usov <dmitry.usov@yahoo.com>
2025-01-31 13:28:53 +0400

    feat: add LocalStorage

--- src/components/TopControls.tsx
+++ src/components/TopControls.tsx
@@ -1,16 +1,13 @@
 import React, { ReactNode } from 'react';
 
+const LOCAL_STORAGE_ID = 'search-wer433j';
 type State = { query: string; inputValue: string };
-type Validation = { minLength: number; maxLength: number };
+
 export default class TopControls extends React.Component {
   state: Readonly<State> = {
-    query: 'InitQ',
-    inputValue: 'InitI',
+    query: '',
+    inputValue: localStorage.getItem(LOCAL_STORAGE_ID) || '',
   };
-
-  validation: Readonly<Validation> = { minLength: 3, maxLength: 60 };
-
-  changeQuery = (): void => {};
 
   updateInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value: string = event.target.value;
@@ -21,15 +18,11 @@ export default class TopControls extends React.Component {
 
   handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const inputValue: string = this.state.inputValue;
-    if (
-      inputValue.length > this.validation.minLength &&
-      inputValue.length <= this.validation.maxLength
-    ) {
-      this.setState((prevState: State) => {
-        return { ...prevState, query: prevState.inputValue };
-      });
-    }
+    const inputValue: string = this.state.inputValue.trim();
+    this.setState(() => {
+      return { inputValue, query: inputValue };
+    });
+    localStorage.setItem(LOCAL_STORAGE_ID, inputValue);
   };
 
   render(): ReactNode {
@@ -46,10 +39,8 @@ export default class TopControls extends React.Component {
             type="text"
             id="subject"
             name="subject"
+            value={this.state.inputValue}
             onChange={(event) => this.updateInputValue(event)}
-            required
-            minLength={this.validation.minLength}
-            maxLength={this.validation.maxLength}
             placeholder="Your Subject"
             autoComplete="false"
             className="w-full rounded-xl border border-solid border-slate-900 p-3 text-2xl text-black sm:text-3xl dark:border-white dark:text-white"
