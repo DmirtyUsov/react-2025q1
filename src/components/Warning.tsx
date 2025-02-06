a9af1136e791cad61e85361896f4b7ebfe36a763
Dima Usov <dmitry.usov@yahoo.com>
2025-02-06 08:27:19 +0400

    refactor: change Warning class to function

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-
-export default class Warning extends React.Component<{
+type Props = {
   title?: string;
   message?: string;
-}> {
-  render(): React.ReactNode {
-    return (
-      <div
-        className="border-l-4 border-orange-500 bg-orange-100 p-4 text-orange-700"
-        role="alert"
-      >
-        <p className="font-bold">{this.props.title || `Be Warned`}</p>
-        <p>{this.props.message || `Something wrong.`}</p>
-      </div>
-    );
-  }
-}
+};
+const Warning = ({
+  title = 'Be warned',
+  message = 'Something wrong.',
+}: Props) => {
+  return (
+    <div
+      className="border-l-4 border-orange-500 bg-orange-100 p-4 text-orange-700"
+      role="alert"
+    >
+      <p className="font-bold">{title}</p>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Warning;
