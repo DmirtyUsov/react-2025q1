242281ee6a71221b855831586492a1ca4058540a
Dima Usov <dmitry.usov@yahoo.com>
2025-02-06 08:21:10 +0400

    refactor: change Footer class to function

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 
-type State = {
-  isEmulateError: boolean;
+const Footer = () => {
+  const [isEmulateError, setIsEmulateError] = useState<boolean>(false);
+
+  if (isEmulateError) {
+    throw new Error('Simulated error.');
+  }
+  return (
+    <footer id="footer" className="mt-4 bg-teal-700 text-xl text-white">
+      <div className="mx-auto flex max-w-4xl p-4">
+        <button
+          onClick={() => {
+            setIsEmulateError(true);
+          }}
+          className="w-48 cursor-pointer rounded-xl border border-solid border-slate-900 bg-amber-700 p-3 text-white hover:bg-amber-600 active:bg-amber-500"
+        >
+          Simulate Error
+        </button>
+      </div>
+    </footer>
+  );
 };
-export default class Footer extends React.Component {
-  state: Readonly<State> = {
-    isEmulateError: false,
-  };
 
-  handleClick(): void {
-    this.setState((): State => {
-      return { isEmulateError: true };
-    });
-  }
-  render(): React.ReactNode {
-    if (this.state.isEmulateError) {
-      throw new Error('Simulated error.');
-    }
-    return (
-      <footer id="footer" className="mt-4 bg-teal-700 text-xl text-white">
-        <div className="mx-auto flex max-w-4xl p-4">
-          <button
-            onClick={() => {
-              this.handleClick();
-            }}
-            className="w-48 cursor-pointer rounded-xl border border-solid border-slate-900 bg-amber-700 p-3 text-white hover:bg-amber-600 active:bg-amber-500"
-          >
-            Simulate Error
-          </button>
-        </div>
-      </footer>
-    );
-  }
-}
+export default Footer;
