081d51b02556b4d0db4d8b8202c5190964f08012
Dima Usov <dmitry.usov@yahoo.com>
2025-02-06 17:07:20 +0400

    refactor: change App to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,48 @@
-import React, { ReactNode } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Warning from './components/Warning';
 import Footer from './components/Footer';
 import Search from './components/Search';
-import { ApiResponse, apiService } from './api.service';
+
 import CardList from './components/CardList';
-import { Character } from './models';
 
-type State = {
-  searchQuery: string;
-  isLoading: boolean;
-  apiResponse: ApiResponse<Character>;
+import { useApiGetCharacters } from './hooks/useApiGetCharacters';
+
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const { isLoading, isOk, errorMsg, charactersPage } =
+    useApiGetCharacters(searchQuery);
+
+  return (
+    <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+      <Header title="Task1: Class components. Error boundary."></Header>
+
+      <main className="mx-auto max-w-4xl">
+        <Search setSearchQuery={(searchQuery) => setSearchQuery(searchQuery)} />
+
+        <section>
+          {!isLoading && (
+            <h2 className="mb-6 text-center text-3xl font-bold text-slate-900 sm:text-4xl dark:text-white">
+              Results
+            </h2>
+          )}
+          {!isLoading && !isOk && charactersPage?.results && (
+            <Warning title={`Failed to load data`} message={errorMsg} />
+          )}
+          {isLoading === true ? (
+            <h3 className="mb-6 animate-pulse text-center text-3xl">
+              Loading...
+            </h3>
+          ) : (
+            <CardList
+              characters={charactersPage ? charactersPage.results : []}
+            />
+          )}
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
-
-export default class App extends React.Component {
-  state: Readonly<State> = {
-    searchQuery: 'Some Text For Mount Swerrq2',
-    isLoading: false,
-    apiResponse: { isOk: true, errorMsg: '', payload: undefined },
-  };
-
-  setIsLoadingState(isLoading: boolean) {
-    this.setState((prevState: State) => {
-      return { ...prevState, isLoading };
-    });
-  }
-
-  setSearchQuery(searchQuery: string): void {
-    this.setState((prevState: State) => {
-      return prevState.searchQuery === searchQuery
-        ? prevState
-        : { searchQuery };
-    });
-  }
-
-  loadData(): void {
-    if (this.state.isLoading) {
-      return;
-    }
-
-    this.setIsLoadingState(true);
-
-    apiService
-      .getCharacter(this.state.searchQuery)
-      .then((apiResponse: ApiResponse<Character>) => {
-        console.log(apiResponse);
-
-        this.setState((prevState: State): State => {
-          return {
-            ...prevState,
-            apiResponse,
-          };
-        });
-      })
-      .finally(() => this.setIsLoadingState(false));
-  }
-
-  componentDidUpdate(_: unknown, prevState: Readonly<State>): void {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.loadData();
-    }
-  }
-
-  render(): ReactNode {
-    return (
-      <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
-        <Header title="Task1: Class components. Error boundary."></Header>
-
-        <main className="mx-auto max-w-4xl">
-          <Search
-            setSearchQuery={(searchQuery) => this.setSearchQuery(searchQuery)}
-          />
-
-          <section>
-            {!this.state.isLoading && (
-              <h2 className="mb-6 text-center text-3xl font-bold text-slate-900 sm:text-4xl dark:text-white">
-                Results
-              </h2>
-            )}
-            {!this.state.isLoading &&
-              !this.state.apiResponse.isOk &&
-              this.state.apiResponse.payload && (
-                <Warning
-                  title={`Failed to load data`}
-                  message={this.state.apiResponse.errorMsg}
-                />
-              )}
-            {this.state.isLoading === true ? (
-              <h3 className="mb-6 animate-pulse text-center text-3xl">
-                Loading...
-              </h3>
-            ) : (
-              <CardList
-                characters={
-                  this.state.apiResponse.payload
-                    ? this.state.apiResponse.payload.results
-                    : []
-                }
-              />
-            )}
-          </section>
-        </main>
-
-        <Footer />
-      </div>
-    );
-  }
-}
+export default App;
