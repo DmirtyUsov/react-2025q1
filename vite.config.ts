5478c7d3658bfdf402fa66fb47b0d7b059c1df14
Dima Usov <dmitry.usov@yahoo.com>
2025-02-18 23:01:16 +0400

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,4 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    coverage: {
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+    },
+  },
 });
