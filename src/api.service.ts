5cf79824d4860d1cb7bcd233a18bb4dafb7f6a7e
Dima Usov <dmitry.usov@yahoo.com>
2025-02-23 17:57:15 +0400

    feat: add api service based on RTK Query

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,89 +1,29 @@
-import { Character, Page } from './models';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export type ApiResponse<T> = {
-  isOk: boolean;
-  errorMsg: string;
-  payload?: Page<T> | T | undefined;
-};
-
-const API_URL = 'https://rickandmortyapi.com/api';
-const ENDPOINT_CHARACTER = 'character';
-
-class ApiService {
-  private async getData(url: string): Promise<ApiResponse<unknown>> {
-    const apiResponse: ApiResponse<unknown> = {
-      isOk: true,
-      errorMsg: 'No errors',
-      payload: undefined,
-    };
-
-    return await fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Status ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then((data: unknown) => {
-        apiResponse.payload = data;
-        return apiResponse;
-      })
-      .catch((error: Error) => {
-        apiResponse.isOk = false;
-        console.log(error);
-        apiResponse.errorMsg = error.message;
-        return apiResponse;
-      });
-  }
-
-  async getPage(url: string): Promise<ApiResponse<Character>> {
-    let apiUrl = url;
-    if (!url) {
-      apiUrl = `${API_URL}/${ENDPOINT_CHARACTER}`;
-    }
-    return (await this.getData(apiUrl)) as ApiResponse<Character>;
-  }
-
-  async searchCharacters(query: string): Promise<ApiResponse<Character>> {
-    const apiUrl = `${API_URL}/${ENDPOINT_CHARACTER}?name=${query}`;
-    return (await this.getData(apiUrl)) as ApiResponse<Character>;
-  }
-
-  async getCharacters(query: string): Promise<ApiResponse<Character>> {
-    if (query) {
-      return await this.searchCharacters(query);
-    }
-    return this.getPage('');
-  }
-
-  async getCharacter(id: string): Promise<ApiResponse<Character>> {
-    if (!id) {
-      const error: ApiResponse<Character> = {
-        isOk: false,
-        errorMsg: 'Empty id',
-      };
-      return Promise.resolve(error);
-    }
-    const apiUrl = `${API_URL}/${ENDPOINT_CHARACTER}/${id}`;
-    return (await this.getData(apiUrl)) as ApiResponse<Character>;
-  }
-
-  convertCharacters2Csv(characters: Character[]): string {
-    const header = ['name', 'status', 'species', 'type', 'gender', 'origin'];
-    const rows = characters.map((character) => [
-      character.name,
-      character.status,
-      character.species,
-      character.type,
-      character.gender,
-      character.origin.name,
-    ]);
-
-    const csvContent =
-      'data:text/csv;charset=utf-8,' +
-      [header.join(';'), ...rows.map((e) => e.join(';'))].join('\n');
-    return encodeURI(csvContent);
-  }
+export const API_URL = 'https://rickandmortyapi.com/api';
+export const ENDPOINT_CHARACTER = 'character';
+export enum ACTIONS {
+  NewSearch,
+  LoadPage,
 }
+type ArgGetPage = { searchQuery: string; pageUrl: string; action: ACTIONS };
+export const apiService = createApi({
+  reducerPath: 'apiService',
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  endpoints: (builder) => ({
+    getPage: builder.query<unknown, ArgGetPage>({
+      query: (arg) => {
+        const { searchQuery, pageUrl, action } = arg;
+        if (action === ACTIONS.NewSearch) {
+          return `/${ENDPOINT_CHARACTER}?name=${searchQuery}`;
+        }
+        return pageUrl;
+      },
+    }),
+    getCharacter: builder.query({
+      query: (id: string) => `/${ENDPOINT_CHARACTER}/${id}`,
+    }),
+  }),
+});
 
-export const apiService = new ApiService();
+export const { useGetPageQuery, useGetCharacterQuery } = apiService;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { FavoriteSlice } from './favoriteSlice';
+import { apiService } from '../api.service';
 
 export const store = configureStore({
-  reducer: { favorites: FavoriteSlice.reducer },
+  reducer: {
+    [apiService.reducerPath]: apiService.reducer,
+    favorites: FavoriteSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiService.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
